0ea73142d820b86beb850f6d65d5c050dbcccb3e
aakash60-prog6655l <aakashkundusenapati60@gmail.com>
2026-04-01 01:12:58 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Data Quality Analyzer</h1>
+      <p>Your app is now working 🚀</p>
+    </div>
+  );
+}
